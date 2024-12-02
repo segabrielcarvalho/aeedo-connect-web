7c1f69fcb1e6a0a7da84f195b1f64d5688998a08
Moreira-Ruan <ruanmoreira.dev@gmail.com>
2024-12-01 21:30:41 -0300

    Update format date pt-br

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -2,7 +2,7 @@ export function formatDate(dateString: string) {
   let parts = dateString.split('-')
   let hasDay = parts.length > 2
 
-  return new Date(`${dateString}Z`).toLocaleDateString('en-US', {
+  return new Date(`${dateString}`).toLocaleDateString('pt-br', {
     day: hasDay ? 'numeric' : undefined,
     month: 'long',
     year: 'numeric',
